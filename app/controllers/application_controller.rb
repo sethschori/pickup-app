7448455f65b1950631c72dec0f769bf0bb5aaba5
Seth Schori <sethschori@users.noreply.github.com>
2022-06-29 18:30:57 -0400

    Add #current_user, #require_user! helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  include Passwordless::ControllerHelpers
+
+  helper_method :current_user
+  helper_method :require_user!
+
+  private
+
+  def current_user
+    @current_user ||= authenticate_by_session(User)
+  end
+
+  def require_user!
+    return if current_user
+    redirect_to root_path, flash: { error: 'Please sign in before attempting to access this.' }
+  end
 end
