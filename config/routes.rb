79808cc55382c8d9a76984a6daed1d73ce9bff29
Seth Schori <sethschori@users.noreply.github.com>
2022-07-02 19:38:18 -0400

    Set only for users resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pickups
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   passwordless_for :users
-  resources :users
+  resources :users, only: [:show, :create, :new]
 
   # Defines the root path route ("/")
   # root "articles#index"
